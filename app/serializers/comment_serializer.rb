7a5a5224cfed430840212882eb5efc0745d754f5
Jovin Ang <36396502+Jovin-Ang@users.noreply.github.com>
2024-01-05 02:37:45 +0800

    Change meta location to resource object top level

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,16 +1,15 @@
 class CommentSerializer
   include JSONAPI::Serializer
   attributes :body
-  belongs_to :post
-  belongs_to :user
-  has_many :comments_likes, lazy_load_data: true, meta: Proc.new { |comment, params|
-    { count: comment.comments_likes.length }
-  }
-  has_many :comments_votes, lazy_load_data: true, meta: Proc.new { |comment, params|
+  meta do |comment|
     {
-      count: comment.comments_votes.length,
+      likes: comment.comments_likes.length,
       upvotes: comment.comments_votes.select{|comment_vote| comment_vote.vote == 1}.length,
       downvotes: comment.comments_votes.select{|comment_vote| comment_vote.vote == 2}.length
     }
-  }
+  end
+  belongs_to :post
+  belongs_to :user
+  has_many :comments_likes, lazy_load_data: true
+  has_many :comments_votes, lazy_load_data: true
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,6 +2,13 @@ class PostSerializer
   include JSONAPI::Serializer
 
   attributes :title, :body
+  meta do |post|
+    {
+      likes: post.posts_likes.length,
+      upvotes: post.posts_votes.select{|post_vote| post_vote.vote == 1}.length,
+      downvotes: post.posts_votes.select{|post_vote| post_vote.vote == 2}.length,
+    }
+  end
   belongs_to :user
   belongs_to :category
   belongs_to :status
@@ -11,14 +18,6 @@ class PostSerializer
   } do |post|
     post.comments.order(created_at: :desc)
   end
-  has_many :posts_likes, lazy_load_data: true, meta: Proc.new { |post, params|
-    { count: post.posts_likes.length }
-  }
-  has_many :posts_votes, lazy_load_data: true, meta: Proc.new { |post, params|
-    {
-      count: post.posts_votes.length,
-      upvotes: post.posts_votes.select{|post_vote| post_vote.vote == 1}.length,
-      downvotes: post.posts_votes.select{|post_vote| post_vote.vote == 2}.length
-    }
-  }
+  has_many :posts_likes, lazy_load_data: true
+  has_many :posts_votes, lazy_load_data: true
 end
